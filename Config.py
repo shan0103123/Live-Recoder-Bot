bc7ee92c826a465dcdb4c7532b01f17c6d2beb66
shan0103123 <officialdoratoonz@gmail.com>
2025-05-30 18:21:21 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,16 +3,16 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = ""
+BOT_TOKEN = "7587118236:AAGLMZqnYYr9QLNrIsMMhZyPKOxWrHiG0DE"
 # From my.telegram.org/
-API_ID = ""
-API_HASH =  ""
+API_ID = "26378339"
+API_HASH =  "d5b14fef7f38588836b1a8ba762f8f08"
 # For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "969084369").split(" ")]
+OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "7958067256").split(" ")]
 # No time limit for this users
-AUTH_USERS = [969084369, 2800583882]
+AUTH_USERS = [7958067256, 7592230837]
 # Time gap after each request (in seconds) 
 TIME_GAP = 0
-BOT_USERNAME='test' #bot username without@
+BOT_USERNAME='DT_Manage_Bot' #bot username without@
 API_TOKEN='b7d9e1f7-12a6-4b8e-89a2-c3d124e5f6a7' #don't change this without developer permission
 
